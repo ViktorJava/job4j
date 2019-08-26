142e1a0f83c38f4426fd173c0f6d9dd9b0059708
ViktorJava <gipsyscrew@yandex.ru>
2019-08-26 09:27:30 +0200

    Добавлен класс DummtBot. Класс покрыл Unit тестами

---
+++ chapter_001/src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,36 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DummyBotTest {
+    @Test
+    public void whenGreetBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Привет, Бот"), is("Привет, умник!")
+        );
+
+    }
+
+    @Test
+    public void whenByeBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Пока, Бот"), is("До скорой встречи")
+        );
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        DummyBot bot = new DummyBot();
+        assertThat(
+                bot.answer("Сколько будет 2+2 ?"), is("Это ставит меня в тупик. Задайте другой вопрос")
+
+        );
+
+    }
+
+}

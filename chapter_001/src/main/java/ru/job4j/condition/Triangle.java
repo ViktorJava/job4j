2e0a5bf440e49bdc8958de1a19b1a935558787b7
ViktorJava <gipsyscrew@yandex.ru>
2019-08-27 13:11:07 +0200

    внёс правки в метод exist ();

--- chapter_001/src/main/java/ru/job4j/condition/Triangle.java
+++ chapter_001/src/main/java/ru/job4j/condition/Triangle.java
@@ -4,13 +4,13 @@ package ru.job4j.condition;
  * ТЗ [#173354]: Используя формулу Герона необходимо вычистить площадь треугольника
  *
  * @author Created by ViktorJava on 26.08.2019 (gipsyscrew@gmail.com)
- * @version 0.0.1
+ * @version 0.0.2
  */
 public class Triangle {
 
     /**
      * Метод вычисления полупериметра по длинам сторон.
-     *
+     * <p>
      * Формула.
      * (a + b + c) / 2
      *
@@ -26,24 +26,21 @@ public class Triangle {
     /**
      * Метод проверяет можно ли построить треугольник с такими длинами сторон.
      *
-     * Подумайте какое надо написать условие, чтобы определить можно ли построить треугольник.
-     *
      * @param a Длина от точки a b.
      * @param b Длина от точки a c.
      * @param c Длина от точки b c.
      * @return true or false
      */
     private boolean exist(double a, double b, double c) {
-        if (a >= b + c || b >= a + c || c >= a + b) return false;
-        else return true;
+        return (a < b + c && b < a + c && c < a + b);
     }
 
     /**
      * Метод должен вычислить площадь треугольника.
-     *
+     * <p>
      * Формула.
      * √ p *(p - a) * (p - b) * (p - c)
-     *
+     * <p>
      * где √ - корень квадратный, для извлечения корня использовать метод Math.sqrt().
      *
      * @return Вернуть площадь, если треугольник существует или -1.

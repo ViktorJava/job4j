321cb3776d20c3769e6a0120e918310c7d125c37
ViktorJava <gipsyscrew@yandex.ru>
2019-08-31 06:19:17 +0200

    5.1. Подсчет суммы чётных чисел в диапазоне [#173359] реализована бизнес-логика метода add добавленны комментарии кода

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -19,7 +19,11 @@ public class Counter {
      */
     public int add(int start, int finish) {
         int sum = 0;
-
+        for (start = 0; start <= finish; start++) {
+            if ((start % 2) == 0) {
+                sum += start;
+            }
+        }
         return sum;
     }
 
--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -1,15 +1,22 @@
 package ru.job4j.loop;
 
 import org.junit.Test;
-
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
+ * Класс Unit (JUnit framework) тестов в связке с библиотекой hamcrest
+ *
  * @author Created by ViktorJava on 31.08.2019 (gipsyscrew@gmail.com)
  * @version 0.0.1
  */
 public class CounterTest {
+
+    /**
+     * Тест подсчет суммы чётных чисел в диапазоне от 0 до 10.
+     * Ожидаемое значение 30
+     * В предикат-функции assertThat применяется hamcrest декоратор "is"
+     */
 
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {

45f4aa40192cda9f9bfcfb643d95bd2caef5df5c
ViktorJava <gipsyscrew@yandex.ru>
2019-09-05 17:37:51 +0200

    5.8. JavaDoc. Code convention[#173362]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,27 +1,60 @@
 package ru.job4j.calculator;
-
+/**
+ * Калькулятор. Выполняет простые арифметические действия над
+ * числами с плавающей запятой
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 05.09.2019
+ */
 public class Calculator {
-
+    /**
+     * Сложение двух числе и вывод суммы на консоль
+     *
+     * @param first первое слагаемое
+     * @param second второе слагаемое
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Деление двух числе и вывод частного на консоль
+     *
+     * @param first делимое
+     * @param second делитель
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Умножение двух числе и вывод произведения на консоль
+     *
+     * @param first первый множитель
+     * @param second второй множитель
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Вычитание двух числе и вывод разности на консоль
+     *
+     * @param first уменьшаемое
+     * @param second вычитаемое
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Точка входа в класс
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Package for calculate task.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 05.09.2019
+ */
+package ru.job4j.calculator;

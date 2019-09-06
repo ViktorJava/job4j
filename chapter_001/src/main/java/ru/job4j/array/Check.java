86aee112ff8f59ea0b371091e4d4e800b2747e05
ViktorJava <gipsyscrew@yandex.ru>
2019-09-06 17:30:06 +0200

    6.3. Массив заполнен true или false[#173373]

---
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -0,0 +1,28 @@
+package ru.job4j.array;
+
+/**
+ * ТЗ [#173373]: 6.3. Массив заполнен true или false
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 06.09.2019
+ */
+public class Check {
+
+    /**
+     * Метод должен проверить, что все элементы в массиве являются или true или false
+     *
+     * @param data массив данных
+     * @return true/false
+     */
+    public boolean mono(boolean[] data) {
+        boolean result = false;
+        for (int index = 0; index < data.length; index++) {
+            if (data[0] != data[index]) {
+                result = false;
+                break;
+            } else result = true;
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 06.09.2019
+ */
+public class CheckTest {
+
+    /**
+     * Тест программы для четного кол-ва элем-ов в массиве
+     */
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        Check check = new Check();
+        boolean[] input = new boolean[]{true, true, true,true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
+    }
+
+    /**
+     * Тест программы для нечетного кол-ва элем-ов в массиве
+     */
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        Check check = new Check();
+        boolean[] input = new boolean[]{true, false, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+
+}

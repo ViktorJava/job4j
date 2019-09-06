b9a896482264e5663157b30fd534ee782596fa83
ViktorJava <gipsyscrew@yandex.ru>
2019-09-06 12:52:16 +0200

    косметические улучшения

---
+++ chapter_001/src/main/java/ru/job4j/condition/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *  Package for condition task (задачи по условиям).
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 06.09.2019
+ */
+package ru.job4j.condition;
---
+++ chapter_001/src/main/java/ru/job4j/loop/package-info.java
@@ -0,0 +1,8 @@
+/**
+ *  Package for loop task (задачи циклов).
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 06.09.2019
+ */
+package ru.job4j.loop;

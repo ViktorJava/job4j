9aea882d89c37dffa417bec3ed4c14f1d95bc1f5
ViktorJava <gipsyscrew@yandex.ru>
2019-09-07 05:52:27 +0200

    6.4.1. Слово заканчивается на... [#173380] устранил замечания

--- chapter_001/src/main/java/ru/job4j/array/EndsWith.java
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -17,11 +17,9 @@ public class EndsWith {
      * @return true/false
      */
     public static boolean endsWith(char[] word, char[] post) {
-        boolean result = false;
+        boolean result = true;
         for (int index = 0; index < post.length; index++) {
-            if (post[post.length - 1 - index] == word[word.length - 1 - index]) {
-                result = true;
-            } else {
+            if (post[post.length - 1 - index] != word[word.length - 1 - index]) {
                 result = false;
                 break;
             }
--- chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -29,9 +29,9 @@ public class EndsWithTest {
     @Test
     public void whenNotFinishedWithPostfixThenFalse() {
         char[] word = {'H', 'e', 'l', 'l', 'o' };
-        char[] post = {'l', 'l', 'o' };
+        char[] post = {'l', 'l', 'i' };
         boolean result = EndsWith.endsWith(word, post);
-        assertThat(result, is(true));
+        assertThat(result, is(false));
     }
 
 }

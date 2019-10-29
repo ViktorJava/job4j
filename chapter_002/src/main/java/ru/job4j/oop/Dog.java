8b798f267fb8f5efab3b449131e082927aff8308
ViktorJava <gipsyscrew@yandex.ru>
2019-10-29 21:44:37 +0200

    Котики обормотики.[#173488]

---
+++ chapter_002/src/main/java/ru/job4j/oop/Dog.java
@@ -0,0 +1,18 @@
+package ru.job4j.oop;
+
+/**
+ * ТЗ [#173488]:Котики обормотики.
+ * Создайте класс Dog,создайте три объекта класса Dog.
+ * Присвойте им имена polkan, sharik, zhychka.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 29.10.2019
+ */
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+    }
+}

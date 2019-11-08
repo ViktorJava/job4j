7ecd8e5cdc313de53bc038bca8acddc16e7252a2
ViktorJava <gipsyscrew@yandex.ru>
2019-11-08 15:41:56 +0200

    Data model [#174683] technical passport

---
+++ chapter_002/src/main/java/ru/job4j/pojo/License.java
@@ -0,0 +1,47 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+/**
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 08.11.2019
+ */
+public class License {
+    private String owner;
+    private String model;
+    private String code;
+    private Date created;
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+}

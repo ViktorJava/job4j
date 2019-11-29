9ffb3fbf51c82c2fa9273338a1da2b5f0484c97b
ViktorJava <gipsyscrew@yandex.ru>
2019-11-29 11:27:18 +0200

    Интерфейс Input [#181092]

---
+++ chapter_002/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,26 @@
+package ru.job4j.poly;
+
+/**
+ * Реализация интерфейса Transport
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 29.11.2019
+ */
+public class Bus implements Transport {
+    @Override
+    public void move() {
+        System.out.println("Поехали...");
+    }
+
+    @Override
+    public void passengers(int amount) {
+        System.out.println("Загрузка пассажиров.");
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        int price = 30;//цена за литр топлива
+        return fuel * price;
+    }
+}

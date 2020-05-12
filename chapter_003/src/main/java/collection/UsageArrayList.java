84d4e787d5b0db20c37192dde304b55b77fd566a
ViktorJava <gipsyscrew@yandex.ru>
2020-05-12 22:13:03 +0300

    #173443 Создал класс UsageArrayList. 1. Создал метод main. 2. В методе main создал объект ArrayList с типом String. 3. Добавил 3 имени: Petr, Ivan, Stepan. 4. Далее через цикл for-each вывел все элементы на консоль. 5. Создал модуль chapter_003 в pom.xml.

---
+++ chapter_003/src/main/java/collection/UsageArrayList.java
@@ -0,0 +1,28 @@
+package collection;
+
+import java.util.ArrayList;
+
+/**
+ * Создайте класс UsageArrayList.
+ * В нем создайте метод main.
+ * В методе main создайте объект ArrayList с типом String.
+ * Добавьте в него 3 имени: Petr, Ivan, Stepan.
+ * Далее через цикл for-each выведите все элементы на консоль.
+ * #173443
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 12.05.2020
+ */
+public class UsageArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> people = new ArrayList<String>();
+        people.add("Petr");
+        people.add("Ivan");
+        people.add("Stepan");
+
+        for (Object value : people) {
+            System.out.println(value);
+        }
+    }
+}

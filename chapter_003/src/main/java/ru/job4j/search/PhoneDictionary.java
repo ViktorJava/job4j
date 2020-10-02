32229225c91d2f43604022e9c0ca60d9aa21ef6f
ViktorJava <gipsyscrew@gmail.com>
2020-10-02 14:01:52 +0300

    Телефонный справочник.
    1.Список абонентов фильтруется по ключу.
    2.Метод find() использует функцию высшего порядка.

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,24 +1,25 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 /**
  * Телефонный справочник на базе ArrayList.
- * Поиск данных в списе.
+ * Поиск ключа в списке реализован на лямбде и
+ * функциональном интерфейсе.
  * Модель справочника.
- * [#173439]
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
- * @version 0.1
- * @since 12.05.2020
+ * @version 0.2
+ * @since 2.10.2020
  */
 public class PhoneDictionary {
-    private ArrayList<Person> persons = new ArrayList<Person>();
+    private final ArrayList<Person> persons = new ArrayList<>();
 
     /**
      * Добавить персону в справочник.
      *
-     * @param person персона добавляемя в справочник.
+     * @param person персона добавляемая в справочник.
      */
     public void add(Person person) {
         this.persons.add(person);
@@ -31,12 +32,13 @@ public class PhoneDictionary {
      * @return Список подошедших пользователей.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = t -> t.getName().contains(key)
+                || t.getAddres().contains(key)
+                || t.getPhone().contains(key)
+                || t.getSurname().contains(key);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getPhone().contains(key)
-                    || person.getAddres().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }

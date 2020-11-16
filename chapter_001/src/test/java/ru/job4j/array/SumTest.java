48bef1e01e9f3465bfcc110e93e81052083e6707
ViktorJava <gipsyscrew@gmail.com>
2020-11-16 21:51:39 +0200

    Сумма элементов массива.

---
+++ chapter_001/src/test/java/ru/job4j/array/SumTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Тест метода суммы первого и последнего элементов массива.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 16.11.2020
+ */
+public class SumTest {
+    @Test
+    public void getSum() {
+        int[] array = {12, 20, 70,};
+        int result = 82;
+        int expected = Sum.getSum(array);
+        assertThat(result, is(expected));
+    }
+}

efd9e06d01af44064d5d530916521a33ad95eda7
ViktorJava <gipsyscrew@gmail.com>
2020-11-18 22:09:23 +0200

    Элементы с нечетными индексами.

---
+++ chapter_001/src/main/java/ru/job4j/array/EvenIndexArray.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+/**
+ * Элементы с четными индексами.
+ * Метод печатает элементы массива, которые имеют нечетные индексы.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 18.11.2020
+ */
+public class EvenIndexArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 != 0) {
+                System.out.println(array[i]);
+            }
+        }
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/EvenIndexArrayTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+/**
+ * Тест метода печатающего элементы нечетных индексов массива.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 18.11.2020
+ */
+public class EvenIndexArrayTest {
+    @Test
+    public void main() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        EvenIndexArray.main(null);
+        String ln = System.lineSeparator();
+        String expected = "1" + ln + "3" + ln + "5" + ln + "7" + ln + "9" + ln;
+        assertThat(out.toString(), is(expected));
+    }
+}

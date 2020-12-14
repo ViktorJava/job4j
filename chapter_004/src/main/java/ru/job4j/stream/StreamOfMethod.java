78ed83bc48fa042f8025a0f8ca9c47ebb782ede6
ViktorJava <gipsyscrew@gmail.com>
2020-12-14 22:53:46 +0200

    Переместил классы из пакета lambda -> stream

--- chapter_004/src/main/java/ru/job4j/stream/StreamOfMethod.java
+++ chapter_004/src/main/java/ru/job4j/stream/StreamOfMethod.java
@@ -1,4 +1,4 @@
-package ru.job4j.lambda;
+package ru.job4j.stream;
 
 import java.util.stream.Stream;
 
--- chapter_004/src/test/java/ru/job4j/stream/StreamBuilderTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/StreamBuilderTest.java
@@ -1,4 +1,4 @@
-package ru.job4j.lambda;
+package ru.job4j.stream;
 
 import org.junit.Test;
 

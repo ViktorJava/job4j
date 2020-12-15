f23381da873dcc74d38d991cb5b0a3c829fb8100
ViktorJava <gipsyscrew@gmail.com>
2020-12-15 23:07:25 +0200

    Сборка элементов. Сборка в List.

---
+++ chapter_004/src/main/java/ru/job4j/stream/CollectToList.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Сборка элементов. Сборка в List.
+ * <p>
+ * Собрать, переданный стрим в список.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 15.12.2020
+ */
+public class CollectToList {
+    /**
+     * Метод сбора переданного стрима целых чисел типа Integer
+     * в ArrayList.
+     *
+     * @param data Стрим целых чисел типа Integer.
+     * @return Собранный из стрима ArrayList.
+     */
+    public static List<Integer> collect(Stream<Integer> data) {
+        return data.collect(Collectors.toList());
+    }
+}

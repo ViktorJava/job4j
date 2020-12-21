73edb68329975411327222643a343a71d49ff1d6
ViktorJava <gipsyscrew@gmail.com>
2020-12-21 23:05:32 +0200

    Сборка элементов. Сборка в произвольную реализацию Collection.

---
+++ chapter_004/src/test/java/ru/job4j/stream/CollectToAnyCollectionTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тест создания из стрима, коллекции LinkedHashSet.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 21.12.2020
+ */
+public class CollectToAnyCollectionTest {
+    @Test
+    public void test() {
+        List<Integer> data = List.of();
+        String expectClass = "java.util.LinkedHashSet";
+        String resultClass = CollectToAnyCollection.collect(data.stream())
+                                                   .getClass()
+                                                   .getName();
+        assertEquals(expectClass, resultClass);
+    }
+}

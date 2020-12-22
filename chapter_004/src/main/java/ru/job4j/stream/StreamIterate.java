a41d4b672c22d2319fe544a485662e2dd7b7c7c1
ViktorJava <gipsyscrew@gmail.com>
2020-12-22 22:12:55 +0200

    Создание стрима. Метод Stream.iterate().

---
+++ chapter_004/src/main/java/ru/job4j/stream/StreamIterate.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import java.util.stream.Stream;
+
+/**
+ * Создание стрима. Метод Stream.iterate().
+ * <p>
+ * Нужно написать метод, чтобы обход начиная с 0,
+ * причем обходился бы только каждый второй элемент.
+ * Например, для {1, 2, 3, 4} вывод должен быть 1 и 3.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 22.12.2020
+ */
+public class StreamIterate {
+    /**
+     * Метод обхода массива данных, типа Integer.
+     *
+     * @param data Массив данных, целочисленного типа.
+     */
+    public static void showArray(Integer[] data) {
+        Stream.iterate(0, i -> i < data.length, i -> i + 2)
+              .forEach(i -> System.out.println(data[i]));
+    }
+}

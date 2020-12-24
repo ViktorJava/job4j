7ccc685ac6c3941cb33afaa99a17d19fc662feee
ViktorJava <gipsyscrew@gmail.com>
2020-12-24 07:30:28 +0200

    Терминальные операции. Метод findFirst().

---
+++ chapter_004/src/test/java/ru/job4j/stream/FindFirstMethodTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тест получения первого элемента из списка данных.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 24.12.2020
+ */
+public class FindFirstMethodTest {
+    @Test
+    public void test() {
+        assertEquals(Optional.empty(), FindFirstMethod.first(List.of()));
+        assertEquals(Optional.of(1), FindFirstMethod.first(List.of(1)));
+    }
+}

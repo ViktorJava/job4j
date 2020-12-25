4c3361ae0924f4dc66e63cb5441eed366fa42d31
ViktorJava <gipsyscrew@gmail.com>
2020-12-25 23:08:20 +0200

    Промежуточные операции. Метод takeWhile().

---
+++ chapter_004/src/main/java/ru/job4j/stream/TakeWhileMethod.java
@@ -0,0 +1,31 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Промежуточные операции. Метод takeWhile().
+ * <p>
+ * Получить из списка только отрицательные элементы.
+ * Элементы исходного списка могут идти в произвольном порядке.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 25.12.2020
+ */
+public class TakeWhileMethod {
+    /**
+     * Метод получения только отрицательных значений из списка элементов
+     * целочисленного типа.
+     *
+     * @param data Integer Список элементов целочисленного типа.
+     * @return Integer Список элементов целочисленного типа,
+     * отрицательного значения.
+     */
+    public static List<Integer> takeWhile(List<Integer> data) {
+        return data.stream()
+                   .sorted()
+                   .takeWhile(n -> n < 0)
+                   .collect(Collectors.toList());
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/stream/TakeWhileMethodTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тест получения списка отрицательных значений целочисленного типа.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 25.12.2020
+ */
+public class TakeWhileMethodTest {
+    @Test
+    public void test() {
+        assertEquals(List.of(-2, -1), TakeWhileMethod.takeWhile(
+                List.of(-1, 2, 3, 4, -2))
+        );
+    }
+}

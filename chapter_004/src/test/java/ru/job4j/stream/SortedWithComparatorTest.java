ededafe3e8c28d736e8f6f0b137d50877018be0c
ViktorJava <gipsyscrew@gmail.com>
2020-12-26 00:46:32 +0200

    Промежуточные операции. Метод sorted() с компаратором.

---
+++ chapter_004/src/test/java/ru/job4j/stream/SortedWithComparatorTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Тест сортировки списка строк по убыванию.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 26.12.2020
+ */
+public class SortedWithComparatorTest {
+    @Test
+    public void test() {
+        assertEquals(List.of("c", "b", "a"), SortedWithComparator.sorted(
+                List.of("a", "b", "c"))
+        );
+    }
+}

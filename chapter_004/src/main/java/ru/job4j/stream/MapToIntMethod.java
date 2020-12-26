3aece6a3bd43be00ca810287319d7c98edfef4d5
ViktorJava <gipsyscrew@gmail.com>
2020-12-26 11:05:05 +0200

    Промежуточные операции. Метод mapToInt(). Сумма кодов символов.

---
+++ chapter_004/src/main/java/ru/job4j/stream/MapToIntMethod.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import java.util.List;
+
+/**
+ * Промежуточные операции. Метод mapToInt(). Сумма кодов символов.
+ * <p>
+ * Преобразовать стрим Character в LongStream, т.е. в стрим кодов символов,
+ * и получить сумму этих кодов.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 26.12.2020
+ */
+public class MapToIntMethod {
+    /**
+     * Метод преобразования стрима Character в сумму кодов этих символов.
+     *
+     * @param characters Character Список данных символьного типа.
+     * @return Сумма кодов символов полученных на входе.
+     */
+    public static long sum(List<Character> characters) {
+        return characters.stream()
+                         .mapToLong(s -> (long) s)
+                         .sum();
+    }
+}

f4303a2805c132ff3b2d5dc6c76331a33389d502
ViktorJava <gipsyscrew@gmail.com>
2020-12-27 02:53:51 +0200

    Выполнение issues #5
    В test в блоке collection заменить создание expected значений на
    укороченный вызов of(..).

--- chapter_003/src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertList2ArrayTest.java
@@ -2,7 +2,7 @@ package ru.job4j.collection;
 
 import org.junit.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -23,7 +23,7 @@ public class ConvertList2ArrayTest {
     @Test
     public void when7ElementsThen9() {
         int[][] result = ConvertList2Array.toArray(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7),
+                List.of(1, 2, 3, 4, 5, 6, 7),
                 3
         );
         int[][] expect = {
@@ -41,7 +41,7 @@ public class ConvertList2ArrayTest {
     @Test
     public void when10ElementsThen12() {
         int[][] result = ConvertList2Array.toArray(
-                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
+                List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10),
                 4
         );
         int[][] expect = {
--- chapter_003/src/test/java/ru/job4j/collection/ConvertListTest.java
+++ chapter_003/src/test/java/ru/job4j/collection/ConvertListTest.java
@@ -3,7 +3,6 @@ package ru.job4j.collection;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -26,7 +25,7 @@ public class ConvertListTest {
         List<int[]> in = new ArrayList<>();
         in.add(new int[]{1});
         in.add(new int[]{2, 3});
-        List<Integer> expect = Arrays.asList(1, 2, 3);
+        List<Integer> expect = List.of(1, 2, 3);
         assertThat(ConvertList.convert(in), is(expect));
     }
 
@@ -40,7 +39,7 @@ public class ConvertListTest {
         in.add(new int[]{1});
         in.add(new int[]{2, 3});
         in.add(new int[]{4, 5, 6});
-        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5, 6);
+        List<Integer> expect = List.of(1, 2, 3, 4, 5, 6);
         assertThat(ConvertList.convert(in), is(expect));
     }
 }

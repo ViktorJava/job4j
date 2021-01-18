4a1f8c7f0f23f7cd7bdc0db1630b965c22efc212
ViktorJava <gipsyscrew@gmail.com>
2021-01-18 19:30:45 +0200

    Заменить deprecated method #28

--- chapter_004/src/test/java/ru/job4j/stream/StreamUsageTest.java
+++ chapter_004/src/test/java/ru/job4j/stream/StreamUsageTest.java
@@ -6,8 +6,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
 
 /**
  * Тест фильтрации списка чисел.

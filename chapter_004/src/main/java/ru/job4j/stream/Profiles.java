daafbe017aa848ed9889819ad51c81171d95c57d
ViktorJava <gipsyscrew@gmail.com>
2021-01-25 20:19:16 +0200

    [#173324] Список адресов. #31
    Написал JavaDoc.

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -5,17 +5,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 /**
- * [#173324] Список адресов.
+ * <h2>[#173324] Список адресов.</h2>
  * В туристической компании появилась задача
  * составить список адресов клиентов.
  * В анкете у клиента, есть адрес проживания.
- * <p>
- * 1. Создать класс Profiles и метод List<Address> collect(List<Profile> profiles);
- * 2. При решении задания нужно использовать Stream API, метод map,
- * метод collect(Collectors.toList());
- * 3. Метод Stream.map - принимает элемент потока и возвращает другой элемент.
- * 4. Список элементов должен быть уникальным.
- * Порядок элементов отсортирован по полю String city.
+ *
+ * <li>Создать класс {@link Profiles} и метод
+ * {@code List<Address> collect(List<Profile> profiles)};
+ * <li>При решении задания нужно использовать {@code Stream API}, метод {@code map},
+ * метод {@code collect(Collectors.toList())};
+ * <li>Метод {@code Stream.map} - принимает элемент потока и возвращает другой элемент.
+ * <li>Список элементов должен быть уникальным.
+ * Порядок элементов отсортирован по полю {@code String city}.
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.2
@@ -24,31 +25,30 @@ import java.util.stream.Collectors;
 public class Profiles {
 
     /**
-     * Метод возвращает список адресов клиентов
-     * (без сортировки с дубликатами).
+     * Метод возвращает список адресов клиентов (без сортировки с дубликатами).
      *
      * @param profiles Анкета клиента.
      * @return Список адресов.
      */
     public static List<Adress> collect(List<Profile> profiles) {
         List<Adress> list = profiles.stream()
-                .map(Profile::getAdress)
-                .collect(Collectors.toList());
+                                    .map(Profile::getAdress)
+                                    .collect(Collectors.toList());
         return list;
     }
 
     /**
-     * Метод возвращающий список с уникальными адресами
-     * проживания (без дубликатов по городу проживания)
+     * Метод возвращающий список с уникальными адресами проживания
+     * (без дубликатов по городу проживания)
      *
      * @param profiles Анкета клиента.
      * @return Уникальный список адресов.
      */
     public static List<Adress> uniquAdressList(List<Profile> profiles) {
         return profiles.stream()
-                .map(Profile::getAdress)
-                .sorted(Comparator.comparing(Adress::getCity))
-                .distinct()
-                .collect(Collectors.toList());
+                       .map(Profile::getAdress)
+                       .sorted(Comparator.comparing(Adress::getCity))
+                       .distinct()
+                       .collect(Collectors.toList());
     }
 }

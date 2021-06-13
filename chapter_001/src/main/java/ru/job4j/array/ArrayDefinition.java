378921dff11aa1b6ff23a81b5106c098101a468b
ViktorJava <gipsyscrew@gmail.com>
2021-06-13 15:05:12 +0300

    Объявление массива #33

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+/**
+ * Массивы данных.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 13.06.2021
+ */
+public class ArrayDefinition {
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+    }
+}

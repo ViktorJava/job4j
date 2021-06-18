23bbf00e01d8cd81794ac2612ccfc089ae8ca4cb
ViktorJava <gipsyscrew@gmail.com>
2021-06-18 11:22:43 +0300

    Двухмерный массив. Вывод на консоль #50
    Done.

---
+++ chapter_001/src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,23 @@
+package ru.job4j.array;
+
+/**
+ * Двухмерный массив. Вывод на консоль.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 18.06.2021
+ */
+public class Sequence {
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 1;
+        array[0][2] = 1;
+        array[1][0] = 1;
+        array[1][1] = 1;
+        array[1][2] = 1;
+        array[2][0] = 1;
+        array[2][1] = 1;
+        array[2][2] = 1;
+    }
+}

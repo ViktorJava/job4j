b8cbd0bc06f76cf5a5ce78a50c0946eff1146a6d
ViktorJava <gipsyscrew@gmail.com>
2021-06-19 23:18:01 +0300

    Сумма чисел #52
    Done.

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -1,21 +1,42 @@
 package ru.job4j.loop;
 
 /**
- * TЗ [#173359]:Подсчет суммы чётных чисел в диапазоне
+ * Подсчет суммы чисел в диапазоне.
  *
  * @author Created by ViktorJava on 31.08.2019 (gipsyscrew@gmail.com)
- * @version 0.0.2
+ * @version 0.1.2
  */
 
 public class Counter {
 
     /**
-     * Метод add возвращает сумму чётных чисел,
-     * из чисел,в диапазоне от start до finish
+     * Метод подсчитывает сумму чисел от start до finish.
      *
-     * @param start  начало диапазона
-     * @param finish конец диапазона
-     * @return сумма чётных чисел
+     * @param start  Начало диапазона.
+     * @param finish конец диапазона.
+     * @return Сумма числе в диапазоне от start до finish.
+     */
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int i = start; i <= finish; i++) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+    }
+
+    /**
+     * Метод add возвращает сумму чётных чисел,
+     * из чисел, в диапазоне от start до finish.
+     *
+     * @param start  Начало диапазона.
+     * @param finish Конец диапазона.
+     * @return Сумма чётных чисел.
      */
     public int add(int start, int finish) {
         int sum = 0;

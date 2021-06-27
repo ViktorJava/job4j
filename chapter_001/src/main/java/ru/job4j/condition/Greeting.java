970855120149842e1ba3eb20aba242a9ebe539fb
ViktorJava <gipsyscrew@gmail.com>
2021-06-27 22:12:27 +0300

    String #55
    Done.

---
+++ chapter_001/src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+/**
+ * String.
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 27.06.2021
+ */
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java! ";
+        System.out.println(idea);
+        System.out.println(idea + "But I am a newbie.");
+        int year = 2021;
+        System.out.println(idea + year);
+    }
+}

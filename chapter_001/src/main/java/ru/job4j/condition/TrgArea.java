6b64b801d2fd25c4ba5f05911d532406f46a75b2
ViktorJava <gipsyscrew@gmail.com>
2021-08-20 13:35:00 +0300

    Площадь треугольника. [#175474 #192019] #58
    Ошибка в формуле.

--- chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/TrgArea.java
@@ -15,7 +15,7 @@ public class TrgArea {
      * @return Площадь треугольника.
      */
     public static double area(double a, double b, double c) {
-        double p = a * b * c / 2;
+        double p = (a + b + c) / 2;
         double rsl = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         return rsl;
     }

47cc5a958d196f79e8dcb34ca069b7713db2b283
ViktorJava <gipsyscrew@gmail.com>
2021-08-24 23:10:04 +0300

    Switch - matcher [#334071] #64.
     Done.

---
+++ chapter_001/src/main/java/ru/job4j/condition/MultipleSwitchWeek.java
@@ -0,0 +1,29 @@
+package ru.job4j.condition;
+
+/**
+ * Switch - matcher [#334071].
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 8/24/2021
+ */
+public class MultipleSwitchWeek {
+    /**
+     * Метод возвращает номер дня недели.
+     *
+     * @param name Название дня недели.
+     * @return Номер дня недели.
+     */
+    public static int numberOfDay(String name) {
+        return switch (name) {
+            case "Понедельник", "Monday" -> 1;
+            case "Вторник", "Tuesday" -> 2;
+            case "Среда", "Wednesday" -> 3;
+            case "Четверг", "Thursday" -> 4;
+            case "Пятница", "Friday" -> 5;
+            case "Суббота", "Saturday" -> 6;
+            case "Воскресение", "Sunday" -> 7;
+            default -> -1;
+        };
+    }
+}

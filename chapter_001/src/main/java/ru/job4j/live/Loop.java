4f5f7117bb41b1159d3c67af90e9330216c40050
ViktorJava <gipsyscrew@gmail.com>
2021-08-24 22:56:40 +0300

    Генерация конструкций в IDEA [#279134] #63
     Done.

---
+++ chapter_001/src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,16 @@
+package ru.job4j.live;
+
+/**
+ * Генерация конструкций в IDEA [#279134].
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 8/24/2021
+ */
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}

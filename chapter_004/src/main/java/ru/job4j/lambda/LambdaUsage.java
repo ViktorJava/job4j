abe8d71ea75e05397c143c5931377a762b368bed
ViktorJava <gipsyscrew@gmail.com>
2021-09-16 19:24:46 +0300

    Лямбда блок [#249208] #90
    Done.

---
+++ chapter_004/src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -0,0 +1,24 @@
+package ru.job4j.lambda;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Лямбда блок [#249208].
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 9/16/2021
+ */
+public class LambdaUsage {
+    public static void main(String[] args) {
+        Comparator<String> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + left + " : " + right);
+            return right.length() - left.length();
+        };
+        List<String> ls = Arrays.asList("four", "one", "three");
+        ls.sort(cmpDescSize);
+        System.out.println(ls);
+    }
+}

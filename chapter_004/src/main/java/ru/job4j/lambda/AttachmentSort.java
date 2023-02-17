7fedb971f0115497b0bc315e2ba79907f67188ca
ViktorJava <gipsyscrew@gmail.com>
2023-02-17 23:31:41 +0300

    Анонимные классы [#214142] #86
    Переписал компаратор.

--- chapter_004/src/main/java/ru/job4j/lambda/AttachmentSort.java
+++ chapter_004/src/main/java/ru/job4j/lambda/AttachmentSort.java
@@ -5,26 +5,24 @@ import java.util.Comparator;
 import java.util.List;
 
 /**
- * Класс реализации компаратора как анонимного inner класса.
+ * Анонимные классы.
+ * Класс реализации компаратора как анонимного класса.
  *
  * @author ViktorJava (gipsyscrew@gmail.com)
  * @version 0.1
  * @since 9/15/2021
  */
 public class AttachmentSort {
-    @SuppressWarnings("rawtypes")
     public static void main(String[] args) {
         List<Attachment> attachments = Arrays.asList(
                 new Attachment("image 2", 35),
                 new Attachment("image 1", 100),
                 new Attachment("image 3", 12)
         );
-        Comparator compSize = new Comparator() {
+        Comparator<Attachment> compSize = new Comparator<>() {
             @Override
-            public int compare(Object o1, Object o2) {
-                Attachment left = (Attachment) o1;
-                Attachment right = (Attachment) o2;
-                return left.getSize() - right.getSize();
+            public int compare(Attachment o1, Attachment o2) {
+                return Integer.compare(o1.getSize(), o2.getSize());
             }
         };
         attachments.sort(compSize);

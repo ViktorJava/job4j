a32c571eab5e90ebff49c800efd2f67844c0e745
ViktorJava <gipsyscrew@gmail.com>
2023-02-17 23:40:20 +0300

    Крест в псевдографике. #129
    Done.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,40 @@
+package ru.job4j.loop;
+
+/**
+ * Крест в псевдографике. [#177182]
+ *
+ * @author ViktorJava (gipsyscrew@gmail.com)
+ * @version 0.1
+ * @since 14.11.2022
+ */
+public class Slash {
+    /**
+     * Метод рисует крест в псевдографике.
+     *
+     * @param size размер креста в символах.
+     */
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                boolean left = cell == row;
+                boolean right = cell == size - row - 1;
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Draw 3");
+        draw(3);
+        System.out.println("Draw 5");
+        draw(5);
+    }
+}

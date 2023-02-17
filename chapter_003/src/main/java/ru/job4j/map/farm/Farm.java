173657d84d8670e343b5ce22e911bd2aea200882
ViktorJava <gipsyscrew@gmail.com>
2023-02-17 23:04:29 +0300

    Ферма #125
    Устранил Checkstyle ошибки.

--- chapter_003/src/main/java/ru/job4j/map/farm/Farm.java
+++ chapter_003/src/main/java/ru/job4j/map/farm/Farm.java
@@ -56,8 +56,12 @@ public class Farm {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
             Animal animal = (Animal) o;
             return Objects.equals(name, animal.name);
         }
